125e224c090de7cf1aaf41fadf19776bb7a15c9b
Dhruv10633 <dhruvbisht1265@gmail.com>
2023-03-23 11:07:33 +0530

    Nothing imp

--- Topics/T6_Sorting/Insertion_Sort.java
+++ Topics/T6_Sorting/Insertion_Sort.java
@@ -5,8 +5,6 @@ public class Insertion_Sort {
     public static void main(String[] args){
         int[] arr=new int[]{1,6,4,2,5,2,3,0,-1};
 
-        
-
         for(int i=1;i<arr.length;i++){
             int s=i-1,e=i;
             while(s>=0 && arr[e]<arr[s]){
