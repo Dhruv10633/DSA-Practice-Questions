8a6a643268d9a9f324d1b12d17e4eeab589b3964
Dhruv10633 <dhruvbisht1265@gmail.com>
2023-03-24 22:53:12 +0530

    Max Score of Good Subarr

--- Questions/Stack and Queues/Q5_Max_score_of_Good_subarr.java
+++ Questions/Stack and Queues/Q5_Max_score_of_Good_subarr.java
@@ -1,3 +1,55 @@
-public class Q5_Max_score_of_Good_subarr {
-    
+// https://leetcode.com/problems/maximum-score-of-a-good-subarray/
+import java.util.*;
+
+class Solution {
+    public int maximumScore(int[] nums, int k) {
+        Stack<Integer> st = new Stack<>();
+
+        int maxS = 0;
+
+        for(int i=0;i<nums.length;i++){
+            while(!st.isEmpty() && nums[st.peek()]>=nums[i]){
+                int index = st.pop();
+                int min = nums[index];
+                int l, r=i-1;
+                if(st.isEmpty()){
+                    l =-1;
+                }
+                else{
+                    l = st.peek();
+                }
+                l++;
+
+                if(l<=k && k<=r){
+                    int width = r-l+1;
+                    // System.out.println(min+" "+width);
+                    maxS = Math.max(maxS, min * width);
+                }
+
+            }
+            st.push(i);
+        }
+
+        while(!st.isEmpty()){
+            int index = st.pop();
+            int min = nums[index];
+            int l, r = nums.length-1;
+            if(st.isEmpty()){
+                l =-1;
+            }
+            else{
+                l = st.peek();
+            }
+            l++;
+
+            if(l<=k && k<=r){
+                int width = r-l+1;
+                // System.out.println(min+" "+width);
+                maxS = Math.max(maxS, min * width);
+            }
+
+        }
+
+        return maxS;
+    }
 }
