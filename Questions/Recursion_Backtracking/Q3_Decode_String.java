8d1ea8c039f11d3961a5524c803690037eddb654
Dhruv10633 <dhruvbisht1265@gmail.com>
2023-04-02 17:16:14 +0530

    Decode Iterative using 2stacks

--- Questions/Recursion_Backtracking/Q3_Decode_String.java
+++ Questions/Recursion_Backtracking/Q3_Decode_String.java
@@ -1,7 +1,8 @@
 // https://leetcode.com/problems/decode-string/
 package Recursion_Backtracking;
+import java.util.*;
 
-class Solution {
+class Solution1 {
 
     //////Recursive
     private int i=0;
@@ -33,5 +34,43 @@ class Solution {
             }
         }
         return new String(res);
+    }
+}
+
+
+class Solution2 {
+    public String decodeString(String s) {
+        String res = "";
+        Stack<Integer> countStack = new Stack<>();
+        Stack<String> resStack = new Stack<>();
+        int idx = 0;
+        while (idx < s.length()) {
+            if (Character.isDigit(s.charAt(idx))) {
+                int count = 0;
+                while (Character.isDigit(s.charAt(idx))) {
+                    count = 10 * count + (s.charAt(idx) - '0');
+                    idx++;
+                }
+                countStack.push(count);
+            }
+            else if (s.charAt(idx) == '[') {
+                resStack.push(res);
+                res = "";
+                idx++;
+            }
+            else if (s.charAt(idx) == ']') {
+                StringBuilder temp = new StringBuilder (resStack.pop());
+                int repeatTimes = countStack.pop();
+                for (int i = 0; i < repeatTimes; i++) {
+                    temp.append(res);
+                }
+                res = temp.toString();
+                idx++;
+            }
+            else {
+                res += s.charAt(idx++);
+            }
+        }
+        return res;
     }
 }
