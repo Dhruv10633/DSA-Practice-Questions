dd2e44ae6185a6627c3d2fd9db8339fe81983bb4
Dhruv10633 <dhruvbisht1265@gmail.com>
2023-04-14 17:03:11 +0530

    Added all possible methods for The Ques

--- Questions/DP/Q4_LCS.java
+++ Questions/DP/Q4_LCS.java
@@ -2,16 +2,28 @@
 package DP;
 
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
+    public int longestPalindromeSubseq(String s) {
+        //We can generate all subsequences and check the longest palindrome anong them using DP but leads to (TLE)
+
+        //Instead we take the string 's' and reverse of 's' and generate their Longest Common Subseq.(LCS)
+        String s2="";
+        for(int i=s.length()-1 ; i>=0 ; i--){
+            s2 += s.charAt(i);
+        }
+
+        int[][] dp = new int[s.length()+1][s2.length()+1];
+
         //Memoization
-        // int[][] dp = new int[text1.length()+1][text2.length()+1];
-        // for(int[] arr: dp){
-        //     Arrays.fill(arr,-1);
+        // for(int[] arr : dp){
+        //     Arrays.fill(arr, -1);
         // }
-        // return solveMem(text1, text2, 0, 0, dp);
+        // return solveMem(s, s2, 0, 0, dp);
+
+        //Tabulation
+        // return solveTab(s, s2, dp);
 
         //Space Opt
-        return solveSpaceOpt(text1, text2);
+        return solveSpaceOpt(s, s2);
     }
 
 
@@ -40,9 +52,25 @@ class Solution {
         return curr[0];
     }
 
-    
-    private int solveMem(String text1, String text2, int i, int j, int[][]dp){
-        if(i==text1.length() || j==text2.length()){
+
+    private int solveTab(String s1, String s2, int[][] dp){
+        for(int i=s1.length()-1 ; i>=0 ; i--){
+            for(int j=s2.length()-1 ; j>=0 ; j--){
+                if(s1.charAt(i) == s2.charAt(j)){
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+
+
+    private int solveMem(String s1, String s2, int i, int j, int[][] dp){
+        if(i>=s1.length() || j>=s2.length()){
             return 0;
         }
 
@@ -50,14 +78,11 @@ class Solution {
             return dp[i][j];
         }
 
-        int maxLen = 0;
-        if(text1.charAt(i)==text2.charAt(j)){
-            maxLen = 1 + solveMem(text1, text2, i+1, j+1, dp);
+        if(s1.charAt(i) == s2.charAt(j)){
+            return dp[i][j] = 1 + solveMem(s1, s2, i+1, j+1, dp);
         }
         else{
-            maxLen = Math.max(solveMem(text1, text2, i+1, j, dp), solveMem(text1, text2, i, j+1, dp));
+            return dp[i][j] = Math.max(solveMem(s1, s2, i+1, j, dp), solveMem(s1, s2, i, j+1, dp));
         }
-
-        return dp[i][j] = maxLen;
     }
 }
