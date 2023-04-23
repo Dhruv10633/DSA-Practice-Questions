6071d85de32ab78edd3a1f4f19b9916ae770aceb
Dhruv10633 <dhruvbisht1265@gmail.com>
2023-04-23 11:14:25 +0530

    Tabulation and memoization approach

--- Questions/DP/Q43_Restore_Array.java
+++ Questions/DP/Q43_Restore_Array.java
@@ -1,16 +1,41 @@
 // https://leetcode.com/problems/restore-the-array/description/
 package DP;
-import java.util.*;
+// import java.util.*;
 
 class Solution {
 
     private int mod = (int)1e9+7;
-    
+
     public int numberOfArrays(String s, int k) {
         int[] dp = new int[s.length()+1];
-        Arrays.fill(dp, -1);
 
-        return solveMem(s, k, 0, dp);
+        //Memoization
+        // Arrays.fill(dp, -1);
+        // return solveMem(s, k, 0, dp);
+
+        //Tabulation
+        return solveTab(s, k, dp);
+    }
+
+
+    private int solveTab(String s, int k, int[] dp){
+        dp[s.length()]=1;
+
+        for(int i=s.length()-1; i>=0; i--){
+
+            long num=0; int ans=0;
+
+            for(int x=i; x<s.length() ;x++){
+                num = num*10 + s.charAt(x)-'0';
+
+                if(num==0 || num>k) break;
+                else ans = (ans + dp[x+1]) % mod;
+
+            }
+            dp[i] = ans;
+        }
+
+        return dp[0];
     }
 
 
@@ -24,9 +49,7 @@ class Solution {
         for(int x=i; x<s.length() ;x++){
             num = num*10 + s.charAt(x)-'0';
 
-            if(num==0 || num>k){
-                return dp[i] = ans;
-            }
+            if(num==0 || num>k) break;
             else ans = (ans + solveMem(s, k, x+1, dp)) % mod;
 
         }
